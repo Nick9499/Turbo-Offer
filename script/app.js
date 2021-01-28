77923fb16ad6ba9c4d203aea8ce96babde9e7ebf
Nick9499 <45527115+Nick9499@users.noreply.github.com>
2021-01-28 15:17:07 +0530

    new nav

--- script/app.js
+++ script/app.js
@@ -1,11 +1,11 @@
 const sellWith = document.getElementById('sellwith')
 const sellDirect = document.getElementById('selldirect')
 
-const sellWithDisappear = () => {
+const sellDirectDisappear = () => {
     sellWith.setAttribute('style', 'display: none;')
     sellDirect.setAttribute('style', 'display: block;')
 }
-const sellDirectDisappear = () => {
+const sellWithDisappear = () => {
     sellDirect.setAttribute('style', 'display: none;')
     sellWith.setAttribute('style', 'display:block;')
 }
